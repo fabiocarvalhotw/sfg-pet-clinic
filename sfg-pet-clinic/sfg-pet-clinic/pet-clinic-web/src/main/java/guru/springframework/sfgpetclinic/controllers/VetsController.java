ef7dd329ac80fd3263eba95b3a292a6ddacc4593
Fábio Carvalho <fabio.carvalho@thoughtworks.com>
2021-05-27 14:07:07 -0300

    Adding Index page and controller

--- sfg-pet-clinic/sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
+++ sfg-pet-clinic/sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class indexController {
+public class VetsController {
 
-    @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
-        return "index";
-    }
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String index(){
+        return "vets/index";    }
 }
